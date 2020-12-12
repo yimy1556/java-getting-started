37eb6de0b1beef47785f45078f33cfd3af4a099d
Yimy Llanos <yimillanos@gmail.com>
2020-12-12 13:40:38 -0300

    test(user): deveria pasar

---
+++ src/test/java/poo/coders/UsersTest.java
@@ -0,0 +1,11 @@
+package poo.coders;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UsersTest {
+    @Test
+    void testDeveriaPasar() {
+        Assertions.assertTrue(false);
+    }
+}
